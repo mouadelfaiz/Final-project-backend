3d4b0bc8d51de349d4cf9d76aed3fa696da11646
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-06 21:38:01 -0400

    install dotenv and creating home route

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-import express from "express"
-
+import express from "express";
+import dotenv from "dotenv"
+dotenv.config();
 const app = express();
 
-
-
+app.get("/", (req, res) => {
+  res.send("This is home page")
+})
 
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`App is listening to port: ${PORT}`)
-})
+  console.log(`App is listening to port: ${process.env.PORT}`);
+});
