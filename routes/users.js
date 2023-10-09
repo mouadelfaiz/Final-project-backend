0b2679f6c745ba5ccd75e5d989fd7af1805d8223
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-08 21:28:23 -0400

    setting up the username and password

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,41 @@
 import express from "express"
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+import { UserModel } from "../modules/Users.js"
+
+const router = express.Router()
+
+router.post("/register", async (req, res) => {
+  const {username, password} = req.body
+
+  const user = await UserModel.findOne({username})
+
+  if (user) {
+    return res.json({ message: "This username already exists!"})
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const newUser = new UserModel({username, password: hashedPassword})
+  await newUser.save()
+
+  res.json({ message: "User Registered Successfully"})
+})
+
+router.post("/login", async (req, res) => {
+  const { username, password} = req.body
+  const user = await UserModel.findOne({ username})
+
+if (!user) {
+  return res.json({message: "This User Doesn't Exist"})
+}
+const isPasswordValid = await bcrypt.compare(password, user.password)
+
+if (!isPasswordValid) {
+  return res.json({message: "User or Password Is Incorrect!"})
+}
+
+const token = jwt.
+})
+
+export {router as userRouter}
--- server.js
+++ server.js
@@ -3,10 +3,14 @@ import dotenv from "dotenv";
 import cors from "cors"
 import mongoose from "mongoose";
 
+import {userRouter} from "./routes/users.js"
+
 const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter)
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log("App connected to database");
