1d6cd8e5000e77cfb57e238513a60aff3d845a50
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-12 07:44:26 -0400

    Update Projects.js

--- modules/Projects.js
+++ modules/Projects.js
@@ -1,12 +1,34 @@
-import mongoose from "mongoose";
+import mongoose, { modelNames } from "mongoose";
 
-
-const ProjectsShema = new mongoose.Schema({
+const ProjectsSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
   },
-  
-})
+  description: {
+    type: String,
+    required: true,
+  },
+  task: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
 
-export const ProjectsModel = mongoose.model("Projects", ProjectsShema)
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+  worktime: {
+    type: Number,
+    required: true,
+  },
+  userOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+export const ProjectsModel = mongoose.model("Projects", ProjectsSchema);
