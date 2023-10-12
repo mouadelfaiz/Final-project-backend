aa5a7b868b561dc3456e9c72ffe10aafe359952e
Mouad El faiz <mouad.elfaiz@gmail.com>
2023-10-12 07:44:30 -0400

    Update Users.js

--- modules/Users.js
+++ modules/Users.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 
 const UserShema = new mongoose.Schema({
   username: {type: String, require: true, unique: true},
-  password:{type: String, require: true}
+  password:{type: String, require: true},
+  savedProjects: [{type: mongoose.Schema.Types.ObjectId, ref: "projects"}]
 })
 
 export const UserModel = mongoose.model("users", UserShema)
